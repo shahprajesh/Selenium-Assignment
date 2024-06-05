fb5ef40dad07cdfa2471c71df0a565edd83891cc
shahprajesh <110009615+shahprajesh@users.noreply.github.com>
2024-06-05 15:22:56 +0530

    Update Assignment5.java

--- src/main/java/ASSIGNMENT/Assignment5.java
+++ src/main/java/ASSIGNMENT/Assignment5.java
@@ -1,2 +1,19 @@
-package ASSIGNMENT;public class Assignment5 {
-}
+package ASSIGNMENT;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Assignment5 {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.w3schools.com/");
+        driver.manage().window().maximize();
+        driver.findElement(By.id("search2")).sendKeys("html");
+        driver.findElement(By.id("learntocode_searchbtn")).click();
+
+
+    }
+    }
+
