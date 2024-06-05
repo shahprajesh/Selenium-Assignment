430101bd5cf92fca1930520e4b95b545708213cc
shahprajesh <110009615+shahprajesh@users.noreply.github.com>
2024-06-05 15:21:18 +0530

    Update Assignment1.java

--- src/main/java/ASSIGNMENT/Assignment1.java
+++ src/main/java/ASSIGNMENT/Assignment1.java
@@ -1,2 +1,24 @@
-package ASSIGNMENT;public class Assignment1 {
+  package ASSIGNMENT;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Assignment1 {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.gecko.driver","src/main/resources/drivers/geckodriver.exe");
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://gmail.com");
+        driver.manage().window().maximize();
+        String title = driver.getTitle(); // Corrected spelling: title
+
+        if (title.equals("Gmail")) {  // Corrected spelling and added closing parenthesis
+            System.out.println("Title is correct");
+        }
+        else {
+            System.out.println("Title is incorrect"); // More descriptive message
+        }
+        driver.quit();
+    }
+}
+
 }
